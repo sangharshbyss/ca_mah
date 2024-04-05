ac78bd95a7678c8297550ab6db857daf1296d876
Sangharsh Shakya <sangharshbyss@gmail.com>
2024-04-05 13:21:56 +0200

    district module removed from pageScrolModule.py

--- pageScrolModule.py
+++ pageScrolModule.py
@@ -4,7 +4,6 @@ so the function starts with checking the act directly.
 2. I think there will be some issue with file writing in csv which can be resolved later.
 """
 
-
 import ws_module_v1
 import logging
 
@@ -19,17 +18,19 @@ poa_dir_year = []
 poa_dir_FIR = []
 poa_dir_date = []
 poa_dir_sec = []
+
+
 def scrollPages(record, name, driver, from_date, to_date):
     number_of_cases_on_all_pages = []
     # this if at first time can be deleted as
     # the function will reach only if there is some record
 
     poa_cases, df = ws_module_v1.check_the_act(driver, poa_dir_district,
-                                           poa_dir_police,
-                                           poa_dir_year,
-                                           poa_dir_FIR,
-                                           poa_dir_date,
-                                           poa_dir_sec)
+                                               poa_dir_police,
+                                               poa_dir_year,
+                                               poa_dir_FIR,
+                                               poa_dir_date,
+                                               poa_dir_sec)
     logger.info('trying to create file')
     ws_module_v1.df_to_file(df=df, name=name, from_date=from_date, to_date=to_date)
     logger.info('file created')
@@ -42,28 +43,28 @@ def scrollPages(record, name, driver, from_date, to_date):
             number_of_cases_on_page = int(len(poa_cases))
             number_of_cases_on_all_pages.append(number_of_cases_on_page)
             ws_module_v1.download_repeat(poa_cases, driver,
-                                             )
+                                         )
             logger.info("finished p1, with PoA")
         if int(record) > 50:
+            # p2
             ws_module_v1.second_page(driver)
             poa_cases, df = ws_module_v1.check_the_act(driver, poa_dir_district,
-                                                   poa_dir_police,
-                                                   poa_dir_year,
-                                                   poa_dir_FIR,
-                                                   poa_dir_date,
-                                                   poa_dir_sec)
-            logger.info('trying to create file')
+                                                       poa_dir_police,
+                                                       poa_dir_year,
+                                                       poa_dir_FIR,
+                                                       poa_dir_date,
+                                                       poa_dir_sec)
+            logger.info('trying to create file of cases')
             ws_module_v1.df_to_file(df=df, name=name, from_date=from_date, to_date=to_date)
             logger.info('file created')
             if not poa_cases:
                 number_of_cases_on_all_pages.append(0)
-                logger.info("finished p2, with PoA")
+                logger.info("finished p2")
             else:
                 number_of_cases_on_page = int(len(poa_cases))
                 number_of_cases_on_all_pages.append(number_of_cases_on_page)
-                ws_module_v1.download_repeat(poa_cases, driver,
-                                             )
-                logger.info("finished p2")
+                ws_module_v1.download_repeat(poa_cases, driver)
+                logger.info("finished p2, with PoA")
         else:
             # logging
             logger.warning(f"{name} finished\n")
@@ -71,31 +72,30 @@ def scrollPages(record, name, driver, from_date, to_date):
             # tell the outer program, all went well.
             return True
 
-
         if int(record) > 100:
             # opening p3
             ws_module_v1.third_page(driver)
             # checking if PoA is available
-            poa_cases = ws_module_v1.check_the_act(driver, poa_dir_district,
+            poa_cases, df = ws_module_v1.check_the_act(driver, poa_dir_district,
                                                    poa_dir_police,
                                                    poa_dir_year,
                                                    poa_dir_FIR,
                                                    poa_dir_date,
                                                    poa_dir_sec)
-            logger.info('trying to create file')
+            logger.info('trying to create file of cases')
             ws_module_v1.df_to_file(df=df, name=name, from_date=from_date, to_date=to_date)
-            logger.info('file created')
+            logger.info('file created for all cases')
             if not poa_cases:
                 number_of_cases_on_all_pages.append(0)
-                #logging
+                # logging
                 logger.info("finished p3")
             else:
                 number_of_cases_on_page = int(len(poa_cases))
                 number_of_cases_on_all_pages.append(number_of_cases_on_page)
-                ws_module_v1.download_repeat(poa_cases, driver,
-                                             )
+                ws_module_v1.download_repeat(poa_cases, driver)
                 # logging
                 logger.info("finished p3, with PoA")
+
 
         else:
             driver.close()
@@ -107,22 +107,25 @@ def scrollPages(record, name, driver, from_date, to_date):
         if int(record) > 150:
             # opening p4
             ws_module_v1.forth_page(driver)
-            poa_cases = ws_module_v1.check_the_act(driver, poa_dir_district,
+            poa_cases, df = ws_module_v1.check_the_act(driver, poa_dir_district,
                                                    poa_dir_police,
                                                    poa_dir_year,
                                                    poa_dir_FIR,
                                                    poa_dir_date,
                                                    poa_dir_sec)
+            logger.info('trying to create file of cases')
+            ws_module_v1.df_to_file(df=df, name=name, from_date=from_date, to_date=to_date)
+            logger.info('file created')
             if not poa_cases:
                 number_of_cases_on_all_pages.append(0)
                 # logging
-                logger.info("finished p4")
+                logger.info('p4 finished with PoA')
+
             else:
                 number_of_cases_on_page = int(len(poa_cases))
                 number_of_cases_on_all_pages.append(number_of_cases_on_page)
                 # downloading FIR
-                ws_module_v1.download_repeat(poa_cases, driver,
-                                             )
+                ws_module_v1.download_repeat(poa_cases, driver)
                 # logging
                 logger.info("finished p4, with PoA")
         else:
@@ -133,14 +136,18 @@ def scrollPages(record, name, driver, from_date, to_date):
             return True
 
         if int(record) > 200:
+            # p5
             ws_module_v1.fifth_page(driver)
             # cheking for PoA
-            poa_cases = ws_module_v1.check_the_act(driver, poa_dir_district,
+            poa_cases, df = ws_module_v1.check_the_act(driver, poa_dir_district,
                                                    poa_dir_police,
                                                    poa_dir_year,
                                                    poa_dir_FIR,
                                                    poa_dir_date,
                                                    poa_dir_sec)
+            logger.info('trying to create file of cases')
+            ws_module_v1.df_to_file(df=df, name=name, from_date=from_date, to_date=to_date)
+            logger.info('file created')
             if not poa_cases:
                 number_of_cases_on_all_pages.append(0)
             else:
@@ -149,6 +156,8 @@ def scrollPages(record, name, driver, from_date, to_date):
                 # downloading FIR
                 ws_module_v1.download_repeat(poa_cases, driver,
                                              )
+                # logging
+                logger.info("finished p5, with PoA")
         else:
             # logging
             logger.warning(f"{name} finished\n")
@@ -157,14 +166,18 @@ def scrollPages(record, name, driver, from_date, to_date):
             return True
 
         if int(record) > 250:
+            # p6
             ws_module_v1.sixth_page(driver)
             # cheking for PoA
-            poa_cases = ws_module_v1.check_the_act(driver, poa_dir_district,
+            poa_cases, df = ws_module_v1.check_the_act(driver, poa_dir_district,
                                                    poa_dir_police,
                                                    poa_dir_year,
                                                    poa_dir_FIR,
                                                    poa_dir_date,
                                                    poa_dir_sec)
+            logger.info('trying to create file of cases')
+            ws_module_v1.df_to_file(df=df, name=name, from_date=from_date, to_date=to_date)
+            logger.info('file for all cass created')
             if not poa_cases:
                 number_of_cases_on_all_pages.append(0)
             else:
@@ -173,6 +186,8 @@ def scrollPages(record, name, driver, from_date, to_date):
                 # downloading FIR
                 ws_module_v1.download_repeat(poa_cases, driver,
                                              )
+                # logging
+                logger.info("finished p6, with PoA")
         else:
             # logging
             logger.warning(f"{name} finished\n")
@@ -181,14 +196,18 @@ def scrollPages(record, name, driver, from_date, to_date):
             return True
 
         if int(record) > 300:
+            # p7
             ws_module_v1.seventh_page(driver)
             # cheking for PoA
-            poa_cases = ws_module_v1.check_the_act(driver, poa_dir_district,
+            poa_cases, df = ws_module_v1.check_the_act(driver, poa_dir_district,
                                                    poa_dir_police,
                                                    poa_dir_year,
                                                    poa_dir_FIR,
                                                    poa_dir_date,
                                                    poa_dir_sec)
+            logger.info('trying to create file of cases')
+            ws_module_v1.df_to_file(df=df, name=name, from_date=from_date, to_date=to_date)
+            logger.info('file for all cass created')
             if not poa_cases:
                 number_of_cases_on_all_pages.append(0)
             else:
@@ -197,6 +216,8 @@ def scrollPages(record, name, driver, from_date, to_date):
                 # downloading FIRs
                 ws_module_v1.download_repeat(poa_cases, driver,
                                              )
+                # logging
+                logger.info("finished p7, with PoA")
         else:
             # loggingg
             logger.warning(f"{name} finished\n")
@@ -205,13 +226,17 @@ def scrollPages(record, name, driver, from_date, to_date):
             return True
 
         if int(record) > 350:
+            # p8
             ws_module_v1.eightth_page(driver)
-            poa_cases = ws_module_v1.check_the_act(driver, poa_dir_district,
+            poa_cases, df = ws_module_v1.check_the_act(driver, poa_dir_district,
                                                    poa_dir_police,
                                                    poa_dir_year,
                                                    poa_dir_FIR,
                                                    poa_dir_date,
                                                    poa_dir_sec)
+            logger.info('trying to create file of cases')
+            ws_module_v1.df_to_file(df=df, name=name, from_date=from_date, to_date=to_date)
+            logger.info('file for all cass created')
             if not poa_cases:
                 # taking output of 0 to list and then to csv
                 number_of_cases_on_all_pages.append(0)
@@ -226,6 +251,8 @@ def scrollPages(record, name, driver, from_date, to_date):
                 ws_module_v1.download_repeat(poa_cases, driver,
                                              )
                 # logging
+                logger.info("finished p8, with PoA")
+                # logging
                 logger.warning(f"{name} finished\n")
                 driver.close()
                 return True
@@ -235,5 +262,5 @@ def scrollPages(record, name, driver, from_date, to_date):
             logger.warning(f"{name} finished\n")
             return True
     except:
-        logger.warning(f"{name} failed. start new program")
+        logger.warning(f"{name} failed. start new program", exc_info=True)
         return False
